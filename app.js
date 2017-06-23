a16910ae318cd0585430d6f66f46ce643c1737fe
dankato <apearmor@gmail.com>
2017-06-22 18:08:57 -0700

    afterhours updates to app

--- app.js
+++ app.js
@@ -20,67 +20,74 @@ const appState = {
 };
 
 
-
 //test URLS
-//https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.75578400000001,-77.263606&radius=5000&type=hospital&keyword=emergency&key=AIzaSyCNb2Rq_psL37TOUxYPnAEt-eFzBrJZe2s
+  //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.75578400000001,-77.263606&radius=5000&type=hospital&keyword=emergency&key=AIzaSyCNb2Rq_psL37TOUxYPnAEt-eFzBrJZe2s
+
+  // https://maps.googleapis.com/maps/api/geocode/json?address=22152&key=AIzaSyCNb2Rq_psL37TOUxYPnAEt-eFzBrJZe2s
 
 
-// userInput is the location that the user set
-
-
-// -------------------------------------------------------------state mods
+// -------------------------------------------------------------controllers
 
 // convets zip code to latitutde and longitude
-let geocoding = function(state, userInput, callback) {
-  console.log(userInput);
+let geocoding = (state, userInput, callback) => {
   let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${userInput}&key=${state.key}`
-  console.log(geocodeURL);
-  $.getJSON(geocodeURL, iHaveThisLocation);
+  const query = {
+    'location': "%s,%s" % (lat, lng),
+  }
+  console.log("query is: " + query);
+  console.log(query.location)
+  $.getJSON(geocodeURL, query, myFunctions.iHaveThisLocation); 
 }
 
 // creates query with longitude and latitude vales for geocoding function and makes API request
-function getData(state, userInput, callback) {
-
+let getData = (state, callback) => {
+  console.log('i am here')
   let searchURL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${appState.geoLocation}&radius=5000&type=hospital&keyword=emergency&key=${state.key}`;
-  $.getJSON(userInput, iHaveResults);
+  function wait(seconds, callback) {
+    setTimeout(function() {
+      $.getJSON(state, myFunctions.iHaveResults);
+    }, seconds * 1000);
+  }
+  console.log('waiting')
+  // $.getJSON(state, myFunctions.iHaveResults);
 }
+// -------------------------------------------------------------state mods
 
+var myFunctions = {
+  iHaveThisLocation: (state, getData, callback) => {
+    state.geoLocation = state.results[0].geometry.location;
+    console.log("longitude and latitude results:" + state.geoLocation);
+    getData(state, callback);
+  },
 
+  storeResults: (state, geocoding) => {
+    appState.results = results.name;
+    console.log("hospital name:" + results.name);
+    render(appState);
+  }
 
-//results.geometry.location
-// https://maps.googleapis.com/maps/api/geocode/json?address=22152&key=AIzaSyCNb2Rq_psL37TOUxYPnAEt-eFzBrJZe2s
-// // State manipulation functions
+};
 
-// Callback
-function iHaveThisLocation(state, getData, callback) {
-  
-  console.log(state, getData)
-
-  state.geoLocation = state.results[0].geometry.location;
-  console.log("longitude and latitude results:" + state.geoLocation);
-  getData(state, callback);
-}
-
-function iHaveResults(state, geocoding) {
-  appState.results = results.name;
-  console.log("hospital name:" + results.name);
-}
 
 //-------------------------------------------------------------------------Render functions
 
 
+
 //------------------------------------------------------------------------- Event handlers
+
 
 $('.search-bar').submit(function (event) {
   event.preventDefault();
   const userInput = $(event.currentTarget).find('input').val();
   console.log('user entered:' + userInput);
-  geocoding(appState, userInput, iHaveThisLocation);
+  geocoding(appState, userInput, myFunctions.iHaveThisLocation);
+  
 })
 
-$(function() {
+$(function () {
   // console.log( "ready?" );
-}) 
+})
+
 
 
 // grab the value -> take the value and add it to the search url -> return we dump in the html
